71722ab2bff29611a970cb7ed4bbb652b355cac2
Shaun Wahle <shaun.wahle@gmail.com>
2017-05-22 14:21:20 -0700

    Objects added in js

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var allImages = [];
+
+function ImageConstruct(imageName, filePath){
+  this.imageName = imageName,
+  this.filePath = filePath,
+  this.timesShown = timesShown,
+  this.timesClicked = timesClicked,
+  allImages.push(this)}
+
+  var bag = ImageConstruct('bag', './images/bag.jpg');
+  var banana = ImageConstruct('banana', './images/banana.jpg');
+  var bathroom = ImageConstruct('bathroom', './images/bathroom.jpg');
+  var boots = ImageConstruct('boots', './images/boots.jpg');
+  var breakfast = ImageConstruct('breakfast', './images/breakfast.jpg');
+  var bubblegum = ImageConstruct('bubblegum', './images/bubblegum.jpg');
+  var chair = ImageConstruct('chair', './images/chair.jpg');
+  var cthulhu = ImageConstruct('cthulhu', './images/cthulhu.jpg');
+  var dogDuck = ImageConstruct('dogDuck', './images/dog-duck.jpg');
+  var dragon = ImageConstruct('dragon', './images/dragon.jpg');
+  var pen = ImageConstruct('pen', './images/pen.jpg');
+  var petSweep = ImageConstruct('petSweep', './images/pet-sweep.jpg');
+  var scisssors = ImageConstruct('scisssors', './images/scisssors.jpg');
+  var shark = ImageConstruct('shark', './images/shark.jpg');
+  var sweep = ImageConstruct('sweep', './images/sweep.jpg');
+  var taunTaun = ImageConstruct('taunTaun', './images/tauntaun.jpg');
+  var unicorn = ImageConstruct('unicorn', './images/unicorn.jpg');
+  var usb = ImageConstruct('usb', './images/usb.jpg');
+  var waterCan = ImageConstruct('waterCan', './images/water-can.jpg');
+  var wineGlass = ImageConstruct('wineGlass', './images/wine-glass.jpg');    
