43fb933a58633dbf4757282ba1aab8a54f3fdafb
Shaun Wahle <shaun.wahle@gmail.com>
2017-05-23 14:52:05 -0700

    new user stories

--- app.js
+++ app.js
@@ -1,100 +1,169 @@
-
-//declaring a busMallImg array and var newClick which grabs #show-imafe
-var busMallImg = [];
+var randomImg = [];
+var runningTotal = [];
+var labelMyImages = [];
+var timesOnScreen = [];
+var timesBeenClicked = [];
 var newClick = document.getElementById('show-image');
-//var counter = 0;
+var onlyClicks = 0;
+//var chartDrawn = false;
 
-var addRight = document.getElementById('add-right');
-var addCenter = document.getElementById('add-center');
-var addLeft = document.getElementById('add-left');
 var showImg = document.getElementById('show-image');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+var myChart = document.getElementById('my-chart');
+var myChart3 = document.getElementById('my-chart3');
 
- //creating a constructor function with the attributes imgName and filePath
-function Image(imgName, filePath) {
+//Img constructor function
+function Picture(imgName, filePath) {
   this.imgName = imgName;
   this.filePath = filePath;
-  this.newPath = 'img/' + this.filePath +'.jpg>';
+  this.newPath = 'img/' + this.filePath + ' />';
   this.timesShown = 0;
   this.timesClicked = 0;
+  //randomImg.push(this);
 }
 
- //pushing in array while also creating elements with constructor function
-busMallImg.push(new Image('bag','bag'));
-busMallImg.push(new Image('banana', 'banana'));
-busMallImg.push(new Image('bathroom', 'bathroom'));
-busMallImg.push(new Image('boots', 'boots'));
-busMallImg.push(new Image('breakfast', 'breakfast'));
-busMallImg.push(new Image('bubblegum', 'bubblegum'));
-busMallImg.push(new Image('chair', 'chair'));
-busMallImg.push(new Image('cthulhu', 'cthulhu'));
-busMallImg.push(new Image('dogDuck', 'dogDuck'));
-busMallImg.push(new Image('dragon', 'dragon'));
-busMallImg.push(new Image('pen', 'pen'));
-busMallImg.push(new Image('petSweep', 'petSweep'));
-busMallImg.push(new Image('scissors', 'scissors'));
-busMallImg.push(new Image('tauntaun', 'tauntaun'));
-busMallImg.push(new Image('unicorn', 'unicorn'));
-busMallImg.push(new Image('usb', 'usb'));
-busMallImg.push(new Image('waterCan', 'waterCan'));
-busMallImg.push(new Image('wineGlass', 'wineGlass'));
+//pushing in array
+randomImg.push(new Picture('bag','bag.jpg'));
+randomImg.push(new Picture('banana', 'banana.jpg'));
+randomImg.push(new Picture('bathroom', 'bathroom.jpg'));
+randomImg.push(new Picture('boots', 'boots.jpg'));
+randomImg.push(new Picture('breakfast', 'breakfast.jpg'));
+randomImg.push(new Picture('bubblegum', 'bubblegum.jpg'));
+randomImg.push(new Picture('chair', 'chair.jpg'));
+randomImg.push(new Picture('cthulhu', 'cthulhu.jpg'));
+randomImg.push(new Picture('dogDuck', 'dogDuck.jpg'));
+randomImg.push(new Picture('dragon', 'dragon.jpg'));
+randomImg.push(new Picture('pen', 'pen.jpg'));
+randomImg.push(new Picture('petSweep', 'petSweep.jpg'));
+randomImg.push(new Picture('scissors', 'scissors.jpg'));
+randomImg.push(new Picture('tauntaun', 'tauntaun.jpg'));
+randomImg.push(new Picture('unicorn', 'unicorn.jpg'));
+randomImg.push(new Picture('usb2', 'usb2.gif'));
+randomImg.push(new Picture('waterCan', 'waterCan.jpg'));
+randomImg.push(new Picture('wineGlass', 'wineGlass.jpg'));
 
-//function writtne that takes a random number and multiply it by the lenthgh of the busMallImg array this will randomize my Images
-function doMath() {
-  return Math.floor(Math.random() * busMallImg.length);
+//Array of random Numbers
+var threeDiffNums = [];
+
+function makeRand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//function written to genertateThree
-//calling my random function and assigning that value to var declared img1
-//declaring liEl1 value equal to document and creating a list item
-//which is then inserted in innerHTML after img scr=
-function generateThree() {
-  var img1 = doMath();
 
-  var divEl = document.createElement('div');
-  divEl.innerHTML = '<img src=' + busMallImg[img1].newPath;
-  busMallImg[img1].timesShown++;
-  showImg.appendChild(divEl);
-
-  var img2 = doMath(Image);
-
-  while (img1 === img2) {
-    img2 = doMath();
+function checkRandom () {
+  for (var i = 0; i < 3; i++) {
+    threeDiffNums.push(makeRand(0, 17));
   }
 
-  var divEl2 = document.createElement('div');
-  divEl2.innerHTML = '<img src=' + busMallImg[img2].newPath;
-  busMallImg[img2].timesShown++;
-  showImg.appendChild(divEl2);
-
-  var img3 = doMath();
-
-  while (img3 === img1 || img3 === img2) {
-    img3 = doMath();
+  while (threeDiffNums[0] === threeDiffNums[1]) {
+    console.log('Duplicate Found');
+    threeDiffNums[1] = makeRand(0,17);
   }
 
-  var divEl3 = document.createElement('div');
-  divEl3.innerHTML = '<img src=' + busMallImg[img3].newPath;
-  busMallImg[img3].timesShown++;
-  showImg.appendChild(divEl3);
-
-  // function increaseTimesClicked () {
-  //   Image.timesClick += 1;
-  // }
+  while (threeDiffNums[2] === threeDiffNums[0] || threeDiffNums[2] === threeDiffNums[1]) {
+    console.log('Duplicate Found');
+    threeDiffNums[2] = makeRand(0,17);
+  }
 }
 
-function handleClick(event) {
-  console.log('you done clicked on ' + event.target.alt);
+function displayImg() {
+  showImg.innerHTML = ' ';
+  //var showImg = document.getElementById('show-image');
+  for (var q = 0; q < threeDiffNums.length; q++) {
+    var divEl = document.createElement('div');
+    divEl.innerHTML = '<img src=' + randomImg[threeDiffNums[q]].newPath;
+    showImg.appendChild(divEl);
+    randomImg[threeDiffNums[q]].timesShown++;
+  }
+}
 
-  for (var q = 0; q < busMallImg.length; q++) {
-    if (event.target.alt === busMallImg[q].imgName) {
-      busMallImg[q].timesClicked++;
+function gatherChartData() {
+  for (z = 0; z < randomImg.length; z++) {
+    labelMyImages.push(randomImg[z].imgName);
+    timesOnScreen.push(randomImg[z].timesShown);
+    timesBeenClicked.push(randomImg[z].timesClicked);
+  }
+}
+
+var data = {
+  labels: labelMyImages,
+  datasets: [
+    {
+      label: 'Times Shown',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: timesOnScreen,
+      yAxisID: 'y-axis-0',
+    },
+    {
+      label: 'Times Selected',
+      backgroundColor: 'rgba(54,162,235,0.2)',
+      borderColor: 'rgba(54,162,235,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(54,162,235,0.4)',
+      hoverBorderColor: 'rgba(54,162,235,1)',
+      data: timesBeenClicked,
     }
-    showImg.innerHTML = ' ';
+  ]
+};
 
-    generateThree();
+function drawChart() {
+  gatherChartData();
+  var ctx = document.getElementById('my-chart3');
+  console.log(labelMyImages, ctx);
+  var resultsChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    //options: options
+  });
+  //myChart3.appendChild(ctx);
+  //chartDrawn = true;
+}
+
+// function hideChart() {
+//   document.getElementById('my-chart3').hidden = true;
+// }
+
+function handleClick() {
+  onlyClicks++;
+  console.log('you done clicked on ' + event.target.alt);
+  for (var q = 0; q < randomImg.length; q++) {
+    if (event.target.alt === randomImg[q].imgName) {
+      randomImg[q].timesClicked++;
+    }
   }
+  threeDiffNums = [];
+  checkRandom();
+  displayImg();
+  if (onlyClicks === 25) {
+    document.getElementById('button2').style.visibility = 'visible';
+    document.getElementById('button3').style.visibility = 'visible';
+    showImg.style.visibility = 'hidden';
+  }
+}
+
+function handleButton3() {
+  console.log('you done clicked on the button');
+  document.getElementById('button2').style.visibility = 'hidden';
+  document.getElementById('button3').style.visibility = 'hidden';
+  showImg.style.visibility = 'visible';
+  onlyClicks = 15;
+}
+
+function handleButton2() {
+  console.log('you are good');
+  drawChart();
 }
 
 newClick.addEventListener('click', handleClick);
+button3.addEventListener('click', handleButton3);
+button2.addEventListener('click', handleButton2);
 
-generateThree();
-handleClick();
+document.getElementById('button2').style.visibility = 'hidden';
+document.getElementById('button3').style.visibility = 'hidden';
+
+checkRandom();
+displayImg();
--- userstories.md
+++ userstories.md
@@ -1,7 +1,23 @@
 Project User Stories:
 
- As the marketing research team, we need a tool to accurately collect and chart feedback from potential customers in order to decide which products are most likely to sell. We'd need this to be random as to not skew opinions.
 
- As a developer, I want to make an adaptable program to randomly generate images from a directory. This will then give the user a chance to choose which item to select by clicking the with an input. This will also collect and store data that the user has selected.
+Developer:
 
- As a user to the site, I want a website that works efficiently and correctly. I also want a display and outline that is attractive and functional.
+As a developer, I want to use object constructors in order to make objects much easier to make.
+As a developer, I want to have clean and efficient code that will be easy to look back and read
+As a developer, I want to give clear, concise, analytical data for the marketing team to use.
+As a developer, I want to create a clean and simple design to give the user a great experience.
+
+Marketing Team:
+
+As the Marketing Team member, I want a basic UX, so data is upfront and clear.
+As the Marketing Team member, I want to be able to easily interpret data for a quick turn around from the survey
+As the Marketing Team member, I want graphical data to show what and how many times an item was selected.
+As the Marketing Team member, I want the user to have a simple and straightforward UI so they have an easy experience.
+
+User:
+
+As a user, I want to see a wide variety of items to better hold my attention.
+As a user, I want something engaging to pass my time while riding the bus.
+As a user, I want items that are visually appealing and grab my attention.
+As a user, I want a visually striking, but not too busy of an interface.
