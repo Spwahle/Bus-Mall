682a5131e41d402fb10ca2af21bf2dba2338fd61
Shaun Wahle <shaun.wahle@gmail.com>
2017-05-23 08:48:05 -0700

    Added click function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-//declaring a randomImg array and var
-var randomImg = [];
+//declaring a busMallImg array and var newClick which grabs #show-imafe
+var busMallImg = [];
 var newClick = document.getElementById('show-image');
 //var counter = 0;
 
@@ -10,87 +10,91 @@ var addLeft = document.getElementById('add-left');
 var showImg = document.getElementById('show-image');
 
  //creating a constructor function with the attributes imgName and filePath
-function Picture(imgName, filePath) {
+function Image(imgName, filePath) {
   this.imgName = imgName;
   this.filePath = filePath;
   this.newPath = 'img/' + this.filePath +'.jpg>';
   this.timesShown = 0;
   this.timesClicked = 0;
-   //randomImg.push(this);
 }
 
  //pushing in array while also creating elements with constructor function
-randomImg.push(new Picture('bag','bag'));
-randomImg.push(new Picture('banana', 'banana'));
-randomImg.push(new Picture('bathroom', 'bathroom'));
-randomImg.push(new Picture('boots', 'boots'));
-randomImg.push(new Picture('breakfast', 'breakfast'));
-randomImg.push(new Picture('bubblegum', 'bubblegum'));
-randomImg.push(new Picture('chair', 'chair'));
-randomImg.push(new Picture('cthulhu', 'cthulhu'));
-randomImg.push(new Picture('dogDuck', 'dogDuck'));
-randomImg.push(new Picture('dragon', 'dragon'));
-randomImg.push(new Picture('pen', 'pen'));
-randomImg.push(new Picture('petSweep', 'petSweep'));
-randomImg.push(new Picture('scissors', 'scissors'));
-randomImg.push(new Picture('tauntaun', 'tauntaun'));
-randomImg.push(new Picture('unicorn', 'unicorn'));
-randomImg.push(new Picture('usb', 'usb'));
-randomImg.push(new Picture('waterCan', 'waterCan'));
-randomImg.push(new Picture('wineGlass', 'wineGlass'));
+busMallImg.push(new Image('bag','bag'));
+busMallImg.push(new Image('banana', 'banana'));
+busMallImg.push(new Image('bathroom', 'bathroom'));
+busMallImg.push(new Image('boots', 'boots'));
+busMallImg.push(new Image('breakfast', 'breakfast'));
+busMallImg.push(new Image('bubblegum', 'bubblegum'));
+busMallImg.push(new Image('chair', 'chair'));
+busMallImg.push(new Image('cthulhu', 'cthulhu'));
+busMallImg.push(new Image('dogDuck', 'dogDuck'));
+busMallImg.push(new Image('dragon', 'dragon'));
+busMallImg.push(new Image('pen', 'pen'));
+busMallImg.push(new Image('petSweep', 'petSweep'));
+busMallImg.push(new Image('scissors', 'scissors'));
+busMallImg.push(new Image('tauntaun', 'tauntaun'));
+busMallImg.push(new Image('unicorn', 'unicorn'));
+busMallImg.push(new Image('usb', 'usb'));
+busMallImg.push(new Image('waterCan', 'waterCan'));
+busMallImg.push(new Image('wineGlass', 'wineGlass'));
 
-
+//function writtne that takes a random number and multiply it by the lenthgh of the busMallImg array this will randomize my Images
 function doMath() {
-  return Math.floor(Math.random() * randomImg.length);
+  return Math.floor(Math.random() * busMallImg.length);
 }
+//function written to genertateThree
+//calling my random function and assigning that value to var declared img1
+//declaring liEl1 value equal to document and creating a list item
+//which is then inserted in innerHTML after img scr=
+function generateThree() {
+  var img1 = doMath();
 
-function genertateThree () {
+  var divEl = document.createElement('div');
+  divEl.innerHTML = '<img src=' + busMallImg[img1].newPath;
+  busMallImg[img1].timesShown++;
+  showImg.appendChild(divEl);
 
-  var img1 = doMath(Picture);
-
-  var liEl1 = document.createElement('li');
-  liEl1.innerHTML = '<img src=' + randomImg[img1].newPath;
-  showImg.appendChild(liEl1);
-
-  var img2 = doMath(Picture);
+  var img2 = doMath(Image);
 
   while (img1 === img2) {
-    img2 = doMath(Picture);
+    img2 = doMath();
   }
 
-  var liEl2 = document.createElement('li');
-  liEl2.innerHTML = '<img src=' + randomImg[img2].newPath;
-  showImg.appendChild(liEl2);
+  var divEl2 = document.createElement('div');
+  divEl2.innerHTML = '<img src=' + busMallImg[img2].newPath;
+  busMallImg[img2].timesShown++;
+  showImg.appendChild(divEl2);
 
-  var img3 = doMath(Picture);
+  var img3 = doMath();
 
   while (img3 === img1 || img3 === img2) {
-    img3 = doMath(Picture);
+    img3 = doMath();
   }
 
-  var liEl3 = document.createElement('li');
-  liEl3.innerHTML = '<img src=' + randomImg[img3].newPath;
-  showImg.appendChild(liEl3);
+  var divEl3 = document.createElement('div');
+  divEl3.innerHTML = '<img src=' + busMallImg[img3].newPath;
+  busMallImg[img3].timesShown++;
+  showImg.appendChild(divEl3);
 
-  function increaseTimesClicked () {
-    Picture.timesClicked += 1;
+  // function increaseTimesClicked () {
+  //   Image.timesClick += 1;
+  // }
+}
+
+function handleClick(event) {
+  console.log('you done clicked on ' + event.target.alt);
+
+  for (var q = 0; q < busMallImg.length; q++) {
+    if (event.target.alt === busMallImg[q].imgName) {
+      busMallImg[q].timesClicked++;
+    }
+    showImg.innerHTML = ' ';
+
+    generateThree();
   }
 }
 
-function handleClick (event) {
-  console.log(event);
-  event.preventDefault ();
+newClick.addEventListener('click', handleClick);
 
-  newClick.addEventListener('click', increaseTimesClicked);
-
-  for (i = 0; i <25; i++) {
-    genertateThree(Picture);
-  }
-}
-
-genertateThree();
- // var li2 = document.createElement('li');
- // li2.innerHTML = '<img src='  pic[index2].path  ' />';
- // appendImg2.appendChild(li2);
-
-doMath();
+generateThree();
+handleClick();
